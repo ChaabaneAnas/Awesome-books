918c961f048e877e4d863805d7f2380f7441854b
ChaabaneAnas <99597333+ChaabaneAnas@users.noreply.github.com>
2022-07-07 15:26:10 +0100

    ESlint

--- nav.js
+++ nav.js
@@ -3,20 +3,20 @@ const Add = document.querySelector('.Add');
 const Contact = document.querySelector('.Contact');
 
 Add.addEventListener('click', () => {
-  document.querySelector('.book-list-container').style.display = "none";
-  document.querySelector('.input').style.display = "block";
-  document.querySelector('.contact').style.display = "none";
-})
+  document.querySelector('.book-list-container').style.display = 'none';
+  document.querySelector('.input').style.display = 'block';
+  document.querySelector('.contact').style.display = 'none';
+});
 
 List.addEventListener('click', () => {
-  document.querySelector('.book-list-container').style.display = "block";
-  document.querySelector('.input').style.display = "none";
-  document.querySelector('.contact').style.display = "none";
+  document.querySelector('.book-list-container').style.display = 'block';
+  document.querySelector('.input').style.display = 'none';
+  document.querySelector('.contact').style.display = 'none';
   location.reload();
-})
+});
 
 Contact.addEventListener('click', () => {
-  document.querySelector('.book-list-container').style.display = "none";
-  document.querySelector('.input').style.display = "none";
-  document.querySelector('.contact').style.display = "block";
-})
+  document.querySelector('.book-list-container').style.display = 'none';
+  document.querySelector('.input').style.display = 'none';
+  document.querySelector('.contact').style.display = 'block';
+});
